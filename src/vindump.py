c92c8a6b7788c6dcd451d2b0fe318443ee215dca
Reginald Maltais <reginald.maltais@gmail.com>
2023-01-06 15:13:52 -0500

    Send output to file for argo output parameter

--- src/vindump.py
+++ src/vindump.py
@@ -3,6 +3,7 @@ Dump the contents of the ``VIN`` field from an input dataset.
 
 The output ends up on ``STDOUT`` to enable pipeline processing using standard unix tools.
 """
+import json
 from pathlib import Path
 
 import pyarrow.parquet as pq
@@ -10,4 +11,7 @@ import pyarrow.parquet as pq
 vrf_file = Path("vrf.parquet")
 
 tbl = pq.read_table(vrf_file)
-print(tbl.column('VIN'))
+vins = tbl.column('VIN').to_pylist()
+
+with open('vins.txt', 'w') as out:
+    json.dump(vins, out)
